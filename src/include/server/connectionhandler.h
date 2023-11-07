cd80d9fda3233e8c4405b40aa7bdcdaa8f861e74
ericling <ericling666@gmail.com>
2023-11-07 09:39:42 +0800

    线程安全的用户id到tcpconnection的映射表

---
+++ src/include/server/connectionhandler.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <muduo/net/TcpConnection.h>
+
+#include <mutex>
+#include <unordered_map>
+
+// 处理用户和TcpConnection的映射
+class ConnectionHandler {
+public:
+    // 添加一个在线用户的TcpConnection
+    void addConnection(int userId, muduo::net::TcpConnectionPtr tcpconn);
+    // 删除一个在线用户的TcpConnection(用于异常断开连接时)
+    int delConnection(muduo::net::TcpConnectionPtr tcpconn);
+    // 删除一个在线用户的TcpConnection(用于用户正常退出登录时)
+    int delConnection(int userId);
+
+    // 单例模式，保证线程安全
+    static ConnectionHandler& instance();
+    ConnectionHandler(const ConnectionHandler&) = delete;
+    ConnectionHandler(ConnectionHandler&&) = delete;
+
+private:
+    // 单例，考虑线程安全
+    ConnectionHandler();
+
+    // 存储在线用户的TcpConnection
+    std::unordered_map<int, muduo::net::TcpConnectionPtr> _userConnMap;
+    // 保证_userConnMap的线程安全，互斥访问
+    std::mutex _connMutex;
+};
---
+++ src/server/connectionhandler.cpp
@@ -0,0 +1,42 @@
+#include "connectionhandler.h"
+
+#include <mutex>
+// 添加一个在线用户的TcpConnection
+void ConnectionHandler::addConnection(int userId,
+                                      muduo::net::TcpConnectionPtr tcpconn) {
+    std::lock_guard<std::mutex> lock(_connMutex);
+    _userConnMap.insert({userId, tcpconn});
+}
+
+// 删除一个即将离线用户的TcpConnection
+int ConnectionHandler::delConnection(muduo::net::TcpConnectionPtr tcpconn) {
+    std::lock_guard<std::mutex> lock(_connMutex);
+    for (auto it = _userConnMap.begin(); it != _userConnMap.end(); ++it) {
+        if (it->second == tcpconn) {
+            int userId = it->first;
+            _userConnMap.erase(it);
+            return userId;
+        }
+    }
+    return -1;  // 未知的TcpConnection
+}
+
+int ConnectionHandler::delConnection(int userId) {
+    std::lock_guard<std::mutex> lock(_connMutex);
+    auto it = _userConnMap.find(userId);
+    if (it != _userConnMap.end()) {
+        _userConnMap.erase(it);
+    }
+    return -1;  // 未知的用户Id
+}
+
+// 获取一个实例对象，线程安全
+ConnectionHandler& ConnectionHandler::instance() {
+    // 静态局部，底层汇编自动加锁解锁，保证线程安全
+    static ConnectionHandler connectionHandler;
+    return connectionHandler;
+}
+
+ConnectionHandler::ConnectionHandler() {
+    // do nothing
+}
